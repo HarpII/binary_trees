a07ad47af2e7e4b21febc94da926c72a8f8aaf90
Harp <walesii19@gmail.com>
2023-03-02 21:49:32 +0000

    fixed code

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,8 +9,8 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->left && node->right)
-		return (1);
-	else
+	if (node == NULL || node->parent != NULL)
 		return (0);
+
+	return (1);
 }
